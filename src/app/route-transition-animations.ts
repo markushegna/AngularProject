5612576243abad36b49d381e6686122e9aa76937
Kim Andre Undal <kim-undal@hotmail.com>
2020-09-30 14:49:34 +0200

    cleana opp i koden

--- src/app/route-transition-animations.ts
+++ src/app/route-transition-animations.ts
@@ -2,112 +2,24 @@ import {animate, animateChild, group, query, style, transition, trigger} from '@
 
 // https://medium.com/ngconf/animating-angular-route-transitions-ef02b871cc30
 export const routeTransitionAnimations = trigger('triggerName',
-  [transition('One => Two, Two => Three, Three => Four', [
+  // Forandra på 'transitions' slepper å ha fleire linjer med kode med '<=>' operasjonen.
+  [transition('One <=> Two, Two <=> Three, Three <=> Four, Four <=> One, Four <=> Two, Four <=> Three, One <=>Two, One<=>Three', [
     style({position: 'relative'}),
     query(':enter, :leave', [
       style({
         position: 'absolute',
         top: 0,
         left: 0,
-        width: '100%'
+        width: '100%',
       })
     ]),
     query(':enter', [style({left: '-100%', opacity: 0})]),
     query(':leave', animateChild()),
     group([
-      query(':leave', [animate('1s ease-out', style({left: '100%', opacity: 0}))]),
-      query(':enter', [animate('1s ease-out', style({left: '0%', opacity: 1}))])
+      query(':leave', [animate('.5s cubic-bezier(.35, 0, .25,1)', style({left: '100%', opacity: 0}))]),
+      query(':enter', [animate('.5s cubic-bezier(.35, 0, .25,1)', style({left: '0%', opacity: 1}))])
     ]),
     query(':enter', animateChild())
-    ]), transition('Four => Three, Three => Two, Two => One, Three => One', [
-    style({position: 'relative'}),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%'
-      })
-    ]),
-    query(':enter', [style({left: '-100%', opacity: 0})]),
-    query(':leave', animateChild()),
-    group([
-      query(':leave', [animate('1s ease-out', style({left: '100%', opacity: 0}))]),
-      query(':enter', [animate('1s ease-out', style({left: '0%', opacity: 1}))])
-    ]),
-    query(':enter', animateChild())
-  ]),
-    transition('One => Two, Two => Three, One => Three, One => Four', [
-      style({position: 'relative'}),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [style({left: '-100%', opacity: 0})]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [animate('1s ease-out', style({left: '100%', opacity: 0}))]),
-        query(':enter', [animate('1s ease-out', style({left: '0%', opacity: 1}))])
-      ]),
-      query(':enter', animateChild())
-    ]),
-    transition('Three => Two, Two => One, Three => One, Two => Four', [
-      style({position: 'relative'}),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [style({left: '-100%', opacity: 0})]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [animate('1s ease-out', style({left: '100%', opacity: 0}))]),
-        query(':enter', [animate('1s ease-out', style({left: '0%', opacity: 1}))])
-      ]),
-      query(':enter', animateChild())
-    ]),
-    transition('Four => Three, Four => Two, Four => One', [
-      style({position: 'relative'}),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [style({left: '-100%', opacity: 0})]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [animate('1s ease-out', style({left: '100%', opacity: 0}))]),
-        query(':enter', [animate('1s ease-out', style({left: '0%', opacity: 1}))])
-      ]),
-      query(':enter', animateChild())
-    ]),
-    transition('Four => Four, Two => Two, One => One, Three => Three', [
-      style({position: 'relative'}),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [style({left: '-100%', opacity: 0})]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [animate('1s ease-out', style({right: '100%', opacity: 0}))]),
-        query(':enter', [animate('1s ease-out', style({left: '0%', opacity: 1}))])
-      ]),
-      query(':enter', animateChild())
     ])
   ])
 ;
