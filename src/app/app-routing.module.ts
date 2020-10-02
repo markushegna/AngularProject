700870473aace1adf28e9633ddd6bc5383344483
Kim Andre Undal <kim-undal@hotmail.com>
2020-10-02 14:04:25 +0200

    La til page not found 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomePageComponent} from './pages/home-page/home-page.component';
 import {AboutComponent} from './pages/about/about.component';
 import {CourseComponent} from './pages/course/course.component';
 import {QuizComponent} from './pages/quiz/quiz.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent, data: {animationState: 'One'}},
+  {path: '', component: HomePageComponent, data: {animationState: 'One'}}, // data: {animationState: 'One'}
   {path: 'about', component: AboutComponent, data: {animationState: 'Two'}},
   {path: 'course', component: CourseComponent, data: {animationState: 'Three'}},
   {path: 'quiz', component: QuizComponent, data: {animationState: 'Four'}},
+  {path: '**', component: PageNotFoundComponent,}
 ];
 
 @NgModule({
@@ -19,4 +21,4 @@ const routes: Routes = [
 export class AppRoutingModule {
 }
 
-export const routingComponents = [HomePageComponent, AboutComponent, CourseComponent, QuizComponent];
+export const routingComponents = [HomePageComponent, AboutComponent, CourseComponent, QuizComponent, PageNotFoundComponent];
